58c3e0219e5f2d0f47c42416fc1d8688f965d6b5
amitakesai <amita.kesai@unitedtalent.com>
2022-12-16 15:55:28 +0530

    add action

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
+const sodium = require('libsodium-wrappers')
+const secretValue = core.getInput('secret-value');
+const secretLevel = core.getInput('secret-level');
+const secretName = core.getInput('secret-name');
+const orgName = core.getInput('org-name');
+const repoName = core.getInput('repo-name');
+const myToken = core.getInput('token');
+const octokit = github.getOctokit(myToken)
+const publicKey = await octokit.request('GET /repos/'+repoName+'/actions/secrets/public-key', {
+  owner: orgName,
+  repo: repoName
+})
 
-
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    //Check if libsodium is ready and then proceed.
+    await sodium.ready;
+    // Convert Secret & Base64 key to Uint8Array.
+    let binkey = sodium.from_base64(publicKey.key, sodium.base64_variants.ORIGINAL)
+    let binsec = sodium.from_string(secretValue)
+    //Encrypt the secret using LibSodium
+    let encBytes = sodium.crypto_box_seal(binsec, binkey)
+    // Convert encrypted Uint8Array to Base64
+    let output = sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL)    
+    if ( secretLevel.toLowerCase() === 'repository') {
+      const result = await octokit.request('PUT /repos/'+repoName+'/actions/secrets/'+secretName, {
+        owner: orgName,
+        repo: repoName,
+        secret_name: secretName,
+        encrypted_value: output,
+        key_id: publicKey.key_id
+      })
+    }
+    core.setOutput('HTTP_STATUS', result.toString());
   } catch (error) {
     core.setFailed(error.message);
   }
