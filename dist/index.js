3e18e0a9999997f1b6737cfd6ac1c153a0c8ce3d
amitakesai <amita.kesai@unitedtalent.com>
2023-01-25 18:56:03 +0530

    code

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,29 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ 9613:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const _sodium = __nccwpck_require__(713)
+const encrypt = async (key, secret) => {
+  if (typeof key !== 'string') {
+    throw new Error('key not a string');
+  }
+  await _sodium.ready;
+  const sodium = _sodium;
+  let binkey = sodium.from_base64(key, sodium.base64_variants.ORIGINAL)
+  let binsec = sodium.from_string(secret)
+  let encBytes = sodium.crypto_box_seal(binsec, binkey)
+  let output = await sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL) 
+  return output;
+  };
+  
+  
+  module.exports = encrypt;
+
+
+/***/ }),
+
 /***/ 7351:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -9702,33 +9725,24 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
-const _sodium = __nccwpck_require__(713)
+const encrypt = __nccwpck_require__(9613);
 const secretValue = core.getInput('secret-value');
 const secretLevel = core.getInput('secret-level');
 const secretName = core.getInput('secret-name');
 const orgName = core.getInput('org-name');
 const repoName = core.getInput('repo-name');
 const token = core.getInput('token');
-
+const envName = core.getInput('env-name');
 const run = async () => {
   try {
     const octokit = github.getOctokit(token);
     //Check if libsodium is ready and then proceed.
-    const publicKey = await octokit.request('GET /repos/'+orgName+'/'+ repoName+'/actions/secrets/public-key', {
-      owner: orgName,
-      repo: repoName
-    })
-    
-    await _sodium.ready;
-    const sodium = _sodium;
-    // Convert Secret & Base64 key to Uint8Array.
-    let binkey = sodium.from_base64(publicKey.data.key, sodium.base64_variants.ORIGINAL)
-    let binsec = sodium.from_string(secretValue)
-    //Encrypt the secret using LibSodium
-    let encBytes = sodium.crypto_box_seal(binsec, binkey)
-    // Convert encrypted Uint8Array to Base64
-    let output = sodium.to_base64(encBytes, sodium.base64_variants.ORIGINAL)    
     if ( secretLevel.toLowerCase() === 'repository') {
+      const publicKey = await octokit.request('GET /repos/'+orgName+'/'+ repoName+'/actions/secrets/public-key', {
+        owner: orgName,
+        repo: repoName
+      })
+      const output = await encrypt(publicKey.data.key,secretValue) 
       const result = await octokit.request('PUT /repos/'+orgName+'/'+ repoName+'/actions/secrets/'+secretName, {
         owner: orgName,
         repo: repoName,
@@ -9737,7 +9751,29 @@ const run = async () => {
         key_id: publicKey.data.key_id
       })
       core.setOutput('message', result.status == 204 ? "Secret Updated" : "Secret Created" );
-    } 
+    } else if ( secretLevel.toLowerCase() === 'environment') {
+      const repoId = await octokit.request('GET /repos/'+orgName+'/'+ repoName, {
+        owner: orgName,
+        repo: repoName
+      });
+      core.info(`Repo Id: ${repoId.data.id}`)
+      const repoPublicKey = await octokit.request('GET /repositories/'+repoId.data.id+'/environments/'+envName+'/secrets/public-key', {
+        repository_id: repoId.data.id,
+        environment_name: envName
+      })
+      core.info(`repo public key: ${repoPublicKey.data.key}`)
+      const output = await encrypt(repoPublicKey.data.key,secretValue)
+      core.info(`Encrypted value: ${output}`);
+      const result = await octokit.request('PUT /repositories/'+repoId.data.id+'/environments/'+envName+'/secrets/'+ secretName, {
+        repository_id: repoId.data.id,
+        environment_name: envName,
+        secret_name: secretName,
+        encrypted_value: output,
+        key_id: repoPublicKey.data.key_id
+      })
+      core.info(`result status: ${result.status}`)
+      core.setOutput('message', result.status == 204 ? "Secret Updated" : "Secret Created" );
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
